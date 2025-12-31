1d848f6c25c646ee1b12fd32d6aee17c4a6de067
ruan-cat <1219043956@qq.com>
2026-01-01 05:09:57 +0800

    📃 docs: 标记维修待办单页面，基本完成检查。进入优化阶段

--- src/pages-sub/repair/dispatch.vue
+++ src/pages-sub/repair/dispatch.vue
@@ -11,6 +11,8 @@
   旧代码：gitee-example/pages/repairDispatch/repairDispatch.vue
 -->
 
+<!-- 基本完成检查审核 -->
+
 <script setup lang="ts">
 import type { RepairOrder } from '@/types/repair'
 import { useRequest } from 'alova/client'
