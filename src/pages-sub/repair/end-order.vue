a8eabd832ca5caee838334ebe9d8299686509fc3
ruan-cat <1219043956@qq.com>
2026-01-06 21:24:07 +0800

    🐳 chore: 标记【结束维修工单】的上级页面

--- src/pages-sub/repair/end-order.vue
+++ src/pages-sub/repair/end-order.vue
@@ -6,6 +6,7 @@
   建议携带参数: ?repairId=REP_001&communityId=COMM_001
 
   完整示例: http://localhost:9000/#/pages-sub/repair/end-order?repairId=REP_001&communityId=COMM_001
+  上级页面: http://localhost:9000/#/pages-sub/repair/order-list
 
   旧代码：gitee-example/pages/repairOrder/repairEnd.vue
 -->
