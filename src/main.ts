128c34fd4ea04a1615ba127ac6a88b9c14254840
ruan-cat <1219043956@qq.com>
2026-01-12 22:03:32 +0800

    🐞 fix!: 处理ku root 组件的错误，手动移除掉在main.ts手动导入的全局根组件。

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { VueQueryPlugin } from '@tanstack/vue-query'
 import { createSSRApp } from 'vue'
-import GlobalKuRoot from './App.ku.vue'
 import App from './App.vue'
 import { requestInterceptor } from './http/interceptor'
 import { routeInterceptor } from './router/interceptor'
@@ -43,9 +42,6 @@ export function createApp() {
   app.use(routeInterceptor)
   app.use(requestInterceptor)
   app.use(VueQueryPlugin)
-
-  // TODO: 经过沟通，此处的手动注册组件属于临时方案 对应的依赖升级并发版后 就可以手动移除了
-  app.component('global-ku-root', GlobalKuRoot)
 
   return {
     app,
